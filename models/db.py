12499490f25d0dcb73a9090d1af75daf51f8a6d5
Odinjor <odinjorud@gmail.com>
2026-04-10 12:59:49 -0400

    Fix: database connection

--- models/db.py
+++ models/db.py
@@ -8,3 +8,18 @@ def get_db_connection():
         database="hospital_db"
     )
     
+def execute_query(query, params=None, fetch=False):
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
+    if params:
+        cursor.execute(query, params)
+    else:
+        cursor.execute(query)
+    if fetch:
+        result = cursor.fetchall()
+    else:
+        db.commit()
+        result = None
+    cursor.close()
+    db.close()
+    return result
